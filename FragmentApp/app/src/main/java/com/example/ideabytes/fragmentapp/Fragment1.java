4237041e36c8588a8e19bad9d3ed9ccf8ee7cb5d
Ideabytes <ideabytes@MacBookPro.local>
2019-01-01 13:49:45 +0530

    fragments practice code

---
+++ FragmentApp/app/src/main/java/com/example/ideabytes/fragmentapp/Fragment1.java
@@ -0,0 +1,21 @@
+package com.example.ideabytes.fragmentapp;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends android.support.v4.app.Fragment {
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater,ViewGroup container,Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+
+}
